a60e28558494d6454713ce98282a0099e5e68ffb
brunodeluk <brunodeluca_96@hotmail.com>
2022-11-06 00:21:25 +0100

    added parser module

---
+++ internal/parser/parser.go
@@ -0,0 +1,40 @@
+package parser
+
+import (
+	"bufio"
+	"strings"
+)
+
+type Node struct {
+	PeeringID string
+	Accepter  string
+	Requester string
+}
+
+func Parse(scanner *bufio.Scanner) []Node {
+	nodes := make([]Node, 0)
+	curNodesCount := -1
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "VPCPEERINGCONNECTIONS") {
+			peeringID := strings.Fields(line)[1]
+			nodes = append(nodes, Node{
+				PeeringID: peeringID,
+			})
+			curNodesCount += 1
+		}
+
+		if strings.HasPrefix(line, "ACCEPTERVPCINFO") {
+			fields := strings.Fields(line)
+			accepter := fields[len(fields)-1]
+			nodes[curNodesCount].Accepter = accepter
+		}
+
+		if strings.HasPrefix(line, "REQUESTERVPCINFO") {
+			fields := strings.Fields(line)
+			requester := fields[len(fields)-1]
+			nodes[curNodesCount].Requester = requester
+		}
+	}
+	return nodes
+}
